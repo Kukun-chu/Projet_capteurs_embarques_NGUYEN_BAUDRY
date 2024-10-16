38e906367e2ba4535713d4c2a798a28165f3c0d5
cannguyen <canhtom.nguyen@gmail.com>
2024-10-16 11:23:14 +0200

    Exercise Timer

--- mbed-os-example-blinky/main.cpp
+++ mbed-os-example-blinky/main.cpp
@@ -14,6 +14,9 @@ void blinking_test();
 void button_test();
 void button_irq();
 void flip_irq();
+void timer();
+void start_counting();
+void end_counting();
 
 // Initialise the digital pin LED1 as an output
 #ifdef LED1
@@ -22,11 +25,18 @@ void flip_irq();
     bool led;
 #endif
 
+// Init Timer
+Timer t;
+
+// Handler timer
+int handlerTimer = 0;
+
 int main()
 {
     //blinking_test();
     //button_test();
-    button_irq();
+    //button_irq();
+    timer();
     return 0;
 }
 
@@ -98,3 +108,35 @@ void flip_irq() {
     led = !led;
 }
 
+void timer() {
+    // Initialise the digital pin BUTTON1
+    #ifdef BUTTON1
+        InterruptIn button(BUTTON1);
+    #else
+        bool button;
+    #endif
+
+    button.rise(&start_counting);  // attach the address of the flip function to the rising edge
+    button.fall(&end_counting);  // attach the address of the flip function to the rising edge
+    while(1) {           // wait around, interrupts will interrupt this!
+        ThisThread::sleep_for(250ms);
+        if(handlerTimer == 1) {
+            printf("End time: %llu milliseconds\n", std::chrono::duration_cast<std::chrono::milliseconds>(t.elapsed_time()).count());
+            handlerTimer = 0;
+        }
+    }
+}
+
+void start_counting() {
+    t.start();
+    led = !led;
+    //printf("Start time: %f seconds\n", t.read());
+}
+
+void end_counting() {
+    t.stop();
+    led = !led;
+    handlerTimer = 1;
+    //printf("End time: %f seconds\n", t.read());
+
+}
